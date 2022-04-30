46b83d793658b5d1c0a83abb518dae829ab74a92
yang vieira <yangflix3@gmail.com>
2022-04-30 17:28:20 -0400

    fix

--- bloco-23/index.js
+++ bloco-23/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
-
+const database = require('./models/authors')
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json())
 
-app.get('/', (req, res, next) => {
-return res.status(200).json({ message: 'bora exercitar' });
+app.get('/', async (req, res, next) => {
+    const data = await database()
+return res.status(200).json(data);
 })
 
 app.listen(PORT, () => console.log('seja bem vindoo'))
