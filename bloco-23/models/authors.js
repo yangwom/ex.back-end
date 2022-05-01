12afe4aaa0717f3d28b4fafb5e9acfe0dff00864
yang vieira <yangflix3@gmail.com>
2022-04-30 21:32:43 -0400

    fix

--- bloco-23/models/authors.js
+++ bloco-23/models/authors.js
@@ -5,8 +5,38 @@ const fullNameAuthor = require('./fullnameAuthors');
 const getAll = async () => {
     const [authors] = await connection.execute('SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
     );
+    console.log(authors);
+
     return authors.map(organizeKeys).map(fullNameAuthor);
 }
 
+const findId = async (id) => {
+    const query = 'SELECT id, first_name, middle_name, last_name FROM model_example.authors WHERE id = ?'
+const [authorsId] = await connection.execute(query, [id])
 
-module.exports = getAll;
+if(!authorsId.length) return null;
+
+return authorsId.map(organizeKeys).map(fullNameAuthor).find(fullNameAuthor)
+
+}
+
+const isValid = (firstName, middleName, lastName) => {
+    if (!firstName || typeof firstName !== 'string') return false;
+    if (!lastName || typeof lastName !== 'string') return false;
+    if (middleName && typeof middleName !== 'string') return false;
+
+    return true;
+};
+
+const create = async (firstName, middleName, lastName) => connection.execute(
+    'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?,?,?)',
+    [firstName, middleName, lastName],
+);
+
+console.log(create)
+module.exports = {
+    getAll,
+    findId,
+    isValid,
+    create,
+};
--- bloco-23/models/fullnameAuthors.js
+++ bloco-23/models/fullnameAuthors.js
@@ -1,6 +1,5 @@
 const getNewauthor = ({ id, firstName, middleName, lastName }) => {
     const fullName = [firstName, middleName, lastName ].filter(Boolean).join(" ")
-    console.log(fullName)
     return {
         id,
         firstName,
