0e643167d1a9ac77205fa8eb0a0d60acc824338e
yang vieira <yangflix3@gmail.com>
2022-05-01 23:24:32 -0400

    data base

---
+++ bloco-23-pratica/database/user.sql
@@ -0,0 +1,10 @@
+USE model_example;
+
+CREATE TABLE user(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name  VARCHAR(45),
+    last_name VARCHAR(45),
+    email VARCHAR(45),
+    password VARCHAR(20)
+    
+);
