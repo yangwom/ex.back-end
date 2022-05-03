0e032f6b996314f7cb459191f86488c27b80164e
yang vieira <yangflix3@gmail.com>
2022-05-02 20:19:48 -0400

    2k

---
+++ bloco-23/bloco-23-pratica/api/middleware/userMiddleware.js
@@ -0,0 +1,35 @@
+const validateName = (req, res, next) => {
+	const { firstName } = req.body;
+	if (!firstName) return next({ status: 400,  error: true, message: 'esse campo é obrigatorio'});
+	next();
+};
+
+const validateLastName = (req, res, next) => {
+	const { lastName } = req.body;
+	if (!lastName) return next({ status: 400,  error: true, message: 'esse campo é obrigatorio'});
+	next();
+};
+
+
+const validateEmail = (req, res, next) => {
+	const { email } = req.body;
+	if (!email) return next({ status: 400, error: true, message: 'esse campo é obrigatorio'});
+	next();
+};
+
+const validatepassword = (req, res, next) => {
+	const { password } = req.body;
+	if (!password) return next({ status: 400, error: true, message: 'esse campo é obrigatorio'});
+	if (password.length < 6) return next({ status: 400, error: true, message: 'no minimo 6 caracteres'});
+	if(typeof password !== 'string') return next({ status: 400, error: true, message: 'NotNumber'});
+	next();
+};
+
+module.exports = {
+	validateName,
+	validateLastName,
+	validateEmail,
+	validatepassword
+};
+
+
---
+++ bloco-23/bloco-23-pratica/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routerUser = require('./api/routers/routerUser');
+const errorMiddleware = require('./api/middleware/errorMiddleware');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+	return res.status(200).json({message: 'seja bemvindo'});
+}); 
+
+app.use('/user', routerUser);
+app.use(errorMiddleware);
+
+app.listen(PORT, ()=> console.log(`ouvindo na porta ${PORT}`));
