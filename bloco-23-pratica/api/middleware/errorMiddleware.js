4317f53195618fce070a90ab3628d67ee411b6f4
yang vieira <yangflix3@gmail.com>
2022-05-02 20:23:46 -0400

    pronto

--- bloco-23-pratica/api/middleware/errorMiddleware.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const errorMiddleware = (err, req, res, next) => {
-	if (err.status) return res.status(err.status).json({ error: err.error, message: err.message });
-	return res.status(500).json({ message: err.message });
-};
-
-
-module.exports = errorMiddleware;
