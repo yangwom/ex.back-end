97d10a80cd0c002094ad05de5e0823cdac5eb6de
yang vieira <yangflix3@gmail.com>
2022-06-08 20:06:31 -0400

    model ts

--- bloco-26-typescript/dia-26-3/express-typescript/model/model.book.ts
+++ bloco-26-typescript/dia-26-3/express-typescript/model/model.book.ts
@@ -1,10 +1,25 @@
 import connection from "./connection";
 import Book from "../interfaces/interfacer";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 
-const getAll = async (): Promise<Book[]> =>  {
-    const query: string = 'SELECT * FROM books'
+export default {
+  async getAll(): Promise<RowDataPacket[]> {
 
-    const [result] = await connection.execute(query)
+    const query: string = "SELECT * FROM books";
 
-    return result as Book[];
-}
+    const [result] = await connection.execute<RowDataPacket[]>(query);
+
+    return result;
+  },
+
+  async create(book:Book): Promise<Book> {
+
+    const query: string = 'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)';
+
+    const { title, price, author, isbn } = book;
+    
+    const [result] = await connection.execute<ResultSetHeader>(query,[title, price, author, isbn]);
+
+    return { id: result.insertId, ...book }
+  }
+};
