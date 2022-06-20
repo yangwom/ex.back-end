17f8a73a8c98ad34dd65b6c12671edfcbbbea960
yang vieira <yangflix3@gmail.com>
2022-06-20 15:01:33 -0400

    fixar

---
+++ bloco-27/abstração.ts
@@ -0,0 +1,41 @@
+class Person2 {
+  name: string;
+  private _weight: number;
+  private _age: number;
+  readonly height: number;
+
+  constructor(name: string, height: number, weight: number, age: number) {
+    this.name = name;
+    this._weight = weight;
+    this._age = age;
+    this.height = height;
+  }
+
+  getWeight() {
+    return this._weight;
+  }
+
+// esta sintaxe permite acessar o valor retornado via person.age (como se fosse um atributo normal)
+  get age() {
+    return this._age;
+  }
+
+// do mesmo modo, esta sintaxe permite modificar o valor com uma simples atribuição: person.age = 42
+  set age(newValue: number) {
+    if (newValue >= 0 && newValue < 200) {
+      this._age = newValue;
+    }
+  }
+
+  birthday() {
+    this._age += 1;
+  }
+
+}
+
+const p1 = new Person2('Maria', 171, 58, 19);
+const p2 = new Person2('João', 175, 201, 20);
+
+p1.name = 'yang'
+p1.age = 199
+console.log(p1.age)
--- bloco-27/exercise-fixar.ts
+++ bloco-27/exercise-fixar.ts
@@ -2,7 +2,7 @@ class Person {
 	name: string;
 	idade: number;
 	cor: string;
-	rg: number;
+	private rg: number;
 
 	constructor(name: string, idade: number, cor: string, rg: number) {
 		this.name = name;
@@ -17,8 +17,8 @@ class Person {
 		console.log(`sua cor ${this.cor}`);
 		console.log(`seu rg ${this.rg}`);
 	}
+
 }
 
 const seusDados = new Person("yang", 22, "preta", 29837345563);
-
 seusDados.idetificar();
